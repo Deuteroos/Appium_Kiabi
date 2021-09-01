9671e0de34f507580e5cf17705f0119b805a8995
JulienDias <JulienDiasGitLab0311>
2021-09-01 10:40:20 +0200

    Final version

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "./src/test/resources/features",
         glue = {"steps"},
-        tags = "@all",
         plugin = {
                 "pretty",
                 "json:target/cucumber-reports/Cucumber.json",
